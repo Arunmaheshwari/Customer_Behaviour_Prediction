38547e5f299eb5dd7bf9827d5d93c02df9aa52fc
Arun Maheshwari <maheshwariarun940@gmail.com>
2024-12-29 15:55:28 -0500

    Created model training files and written code as well

--- steps/model_train.py
+++ steps/model_train.py
@@ -1,17 +1,48 @@
 import logging
-import pandas as pd 
 
-from zenml import step 
+import pandas as pd
+from src.model_dev import (
+    HyperparameterTuner,
+    LightGBMModel,
+    LinearRegressionModel,
+    RandomForestModel,
+    XGBoostModel,
+)
+from sklearn.base import RegressorMixin
+from zenml import step
+from zenml.client import Client
+
+from .config import ModelNameConfig
+
+experiment_tracker = Client().active_stack.experiment_tracker
 
 
-@step
-def train_model(df: pd.DataFrame) -> None:
+@step(experiment_tracker=experiment_tracker.name)
+def train_model(
+    x_train: pd.DataFrame,
+    x_test: pd.DataFrame,
+    y_train: pd.Series,
+    y_test: pd.Series,
+    config: ModelNameConfig,
+) -> RegressorMixin:
     """
-    Trains the model on  the ingested data.
-    
     Args:
-        df: the ingested data
+        x_train: pd.DataFrame
+        x_test: pd.DataFrame
+        y_train: pd.Series
+        y_test: pd.Series
+    Returns:
+        model: RegressorMixin
     """
-    pass
-
+    try:
+        model = None
+        if config.model_name == "LinearRegression":
+            model = LinearRegressionModel()
+            trained_model = model.train(x_train, y_train)
+            return trained_model
+        else:
+            raise ValueError("Model {} not supported".format(config.model_name))
+    except Exception as e:
+        logging.error("Error in traning model: {}".format(e))
+        raise e
     
